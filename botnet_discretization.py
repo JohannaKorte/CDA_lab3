afac29647e2aace125cb293c2903285c9f00bfb9
johanna <johannak12@gmail.com>
2018-06-24 13:34:26 +0200

    Gets data from infected host and visualizes packets and protocol

--- botnet_discretization.py
+++ botnet_discretization.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import csv
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
 
+# PARAMETERS
 downloaded_file = 'capture20110818.pcap.netflow.labeled'
 data_file = 'capture20110818.csv'
+infected_host_ip = '147.32.84.165'
 
 
 def preprocess(inputfile, outputfile):
@@ -22,7 +27,8 @@ def load_data(inputfile):
     data.columns = ["date", "time", "duration", "protocol", "source_IP", "arrow", "destination_IP", "flags", "tos",
                     "packets", "bytes", "flows", "labels"]
     data.insert(0, "datetime", data["date"] + " " + data["time"])  # append date and time variables in one column
-    data.drop(["arrow", "date", "time"], axis=1, inplace=True)     # clean up
+    data.drop(["arrow", "date", "time"], axis=1, inplace=True)     # Remove unnecessary columns
+    data["datetime"] = pd.to_datetime(data.datetime, format="%Y-%m-%d %H:%M:%S.%f")
     return data
 
 
@@ -31,10 +37,36 @@ def remove_background(data):
     data.query("labels != 'Background'", inplace=True)
     return data
 
-#########
+
+def get_host_data(data, ip):
+    """ Gets all dataframe rows from data with as source_IP the given ip address. """
+    host_data = data[data['source_IP'].str.contains(ip)]
+    return host_data
+
+
+def visualize(data):
+    """ Visualizes the network protocol and total packets from the data in the given dataframe. """
+    # Visualize packets
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(data["datetime"], data["packets"], color='#00A6D6')
+    plt.xlabel("Time")
+    plt.ylabel("Number of packets")
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    plt.show()
+    # Visualize protocol
+    plt.figure()
+    pd.value_counts(data["protocol"]).plot.bar(color='#00A6D6')
+    plt.xlabel('Protocol')
+    plt.ylabel('Occurence in counts')
+    plt.show()
+    return
+
 
 if __name__ == '__main__':
     #preprocess(downloaded_file, data_file)
     dataframe = load_data(data_file)
     dataframe = remove_background(dataframe)
+    infected_host_data = get_host_data(dataframe, infected_host_ip)
+    visualize(infected_host_data)
 
